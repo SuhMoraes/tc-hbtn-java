1496b3cf6e346ce0d1b8891f279d098d00be7961
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-01-25 16:43:14 -0300

    Task in Java

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -4,7 +4,7 @@ public class PrintFormatacaoNumeros {
         float valor = 7654.321f;
         NumberFormat locale = NumberFormat.getInstance(new Locale("en", "US"));
         String value = locale.format(valor);
-        System.out.println("Valor: R$ "+ value.substring(0,9));
+        System.out.println("Valor: R$ "+ value.substring(0,8));
         System.out.printf("Taxa: %.2f%s", taxa, "%");
     }
 }
