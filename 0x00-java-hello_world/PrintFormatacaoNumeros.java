222f4171a5401ae2687139e34b96a5e832efbf9a
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-01-25 16:48:16 -0300

    Update PrintFormatacaoNumeros.java

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -2,7 +2,7 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        NumberFormat locale = NumberFormat.getInstance(new Locale("pt","BR"));
+        NumberFormat locale = NumberFormat.getInstance(new Locale("en","US"));
         String value = locale.format(valor);
         System.out.println("Valor: R$ "+ value.substring(0,8));
         System.out.printf("Taxa: %.2f%s", taxa, "%");
