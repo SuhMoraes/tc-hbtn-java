31eec33262691d783e8bf3dbbf97d739695d9f10
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-01-27 10:10:12 -0300

    Task Java

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,17 @@
+import javax.print.attribute.standard.PresentationDirection;
+
+public class Numero {
+    public static void dividir(int a, int b) {
+        int result = 0;
+        try {
+           result = a / b;
+
+        }
+         catch (ArithmeticException ex) {
+             System.out.println("Nao eh possivel dividir por zero");
+         }
+        finally {
+            System.out.println(a + " / " + b + " = " + result);
+        }
+    }
+}
