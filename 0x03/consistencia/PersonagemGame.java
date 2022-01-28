82b1e55653b169d2bc0735bda54761d6c5c121c2
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-01-28 08:32:31 -0300

    Task Java

--- 0x03/consistencia/PersonagemGame.java
+++ 0x03/consistencia/PersonagemGame.java
@@ -1,56 +1,54 @@
-    public class PersonagemGame {
+public class PersonagemGame {
 
-        private int saudeAtual;
-        private String nome;
-        private String status;
+    private String nome;
+    private int saudeAtual;
+    private String status;
 
-        public PersonagemGame(int saudeAtual, String nome) {
-            this.saudeAtual = saudeAtual;
-            this.nome = nome;
-            this.saudeAtual = 0;
-        }
-
-        public void tomarDano(int quantidadeDeDano) {
-            int result = this.saudeAtual -= quantidadeDeDano;
-            if (result < 0) {
-                result = 0;
-            }
-            setSaudeAtual(result);
-
-        }
-
-        public void receberCura(int quantidadeDeCura) {
-            int result = this.saudeAtual += quantidadeDeCura;
-            if (result > 100) {
-                result = 100;
-            }
-            setSaudeAtual(result);
-
-        }
-
-        public int getSaudeAtual() {
-            return saudeAtual;
-        }
-
-        public void setSaudeAtual(int saudeAtual) {
-            this.saudeAtual = saudeAtual;
-            if (this.saudeAtual > 0) {
-                this.status = "vivo";
-            } else {
-                this.status = "morto";
-            }
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.nome = nome;
+        this.saudeAtual = saudeAtual;
+        setSaudeAtual(saudeAtual);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null && nome == " "){
+            this.nome = nome;
+        }
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getStatus() {
+        if (saudeAtual > 0) {
+            status = "vivo";
+        }else{
+            status = "morto";
+        }
+        return status;
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        saudeAtual += quantidadeDeCura;
+        if (saudeAtual > 100){
+            setSaudeAtual(100);
+        }
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        saudeAtual -= quantidadeDeDano;
+        if (saudeAtual <= 0){
+            setSaudeAtual(0);
+        }
+    }
+
+}
