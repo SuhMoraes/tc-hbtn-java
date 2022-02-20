65ae698678f8d0fc1868f871226f288a52afb882
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-02-20 14:56:08 -0300

    Task 01

--- 0x13/resumo_pedido/provedores/JadLog.java
+++ 0x13/resumo_pedido/provedores/JadLog.java
@@ -9,9 +9,9 @@ public class JadLog implements ProvedorFrete{
 
         double valorFrete;
         if ( peso > 2000) {
-            valorFrete  = valor * 0.7;
+            valorFrete  = valor * 0.07;
         } else {
-            valorFrete = valor * 0.45;
+            valorFrete = valor * 0.045;
         }
         return new Frete(TIPO_PROVEDOR_FRETE, valorFrete);
     }
