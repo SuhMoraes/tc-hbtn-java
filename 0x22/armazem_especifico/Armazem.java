bb7118338b6cd0221d1dd03ab467d3a1ac4caf9b
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-03-03 08:38:43 -0300

    Task 01 - Armazem Especifico

---
+++ 0x22/armazem_especifico/Armazem.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class  Armazem<T> implements Armazenavel<T>{
+
+    private Map<String, T> itens = new HashMap<>();
+
+    public Armazem() {
+    }
+
+    public Map<String, T> getItens() {
+        return itens;
+    }
+
+    public void setItens(Map<String, T> itens) {
+        this.itens = itens;
+    }
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        itens.put(nome, valor);
+
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return itens.get(nome);
+    }
+}
