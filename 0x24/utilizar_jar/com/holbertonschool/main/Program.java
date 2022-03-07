5a1d6bcbe6dfa0aedaf33d99fcc530234ded5cd0
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-03-07 15:22:31 -0300

    Task 01

---
+++ 0x24/utilizar_jar/com/holbertonschool/main/Program.java
@@ -0,0 +1,15 @@
+public class Program {
+    public static void main(String[] args) {
+        int soma = Aritmeticos.somar(2, 3);
+        System.out.println(soma);
+
+        int divisao = Aritmeticos.somar(5, 2);
+        System.out.println(divisao);
+
+        int multiplicacao = Aritmeticos.somar(12, 4);
+        System.out.println(multiplicacao);
+
+        int subtracao = Aritmeticos.subtrair(2, 3);
+        System.out.println(subtracao);
+    }
+}
